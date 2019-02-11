348271037662fec7f7b999c1f927adebf2eb5247
vagrant <vagrant@homestead>
2019-02-11 16:20:23 +0000

    fin du lundi

--- routes/web.php
+++ routes/web.php
@@ -12,5 +12,53 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('accueil');
+});
+
+Route::get('/about', function () {
+    return view('about');
+});
+
+Route::get('/blog', function () {
+    return view('blog');
+});
+
+Route::get('/contact', function () {
+    return view('contact');
+});
+
+Route::get('/hotel-room', function () {
+    return view('hotel-room');
+});
+
+Route::get('/hotels', function () {
+    return view('hotels');
+});
+
+Route::get('/message', function () {
+    return view('message');
+});
+
+Route::get('/services', function () {
+    return view('services');
+});
+
+Route::get('/tour-place', function () {
+    return view('tour-place');
+});
+
+Route::get('/tours', function () {
+    return view('tours');
+});
+
+Route::get('/voyages', function () {
+    return view('voyages');
+});
+
+Route::get('/voyage', function () {
+    return view('voyage');
+});
+
+Route::get('/admin', function ($id) {
+    return "administration".$id;
 });
